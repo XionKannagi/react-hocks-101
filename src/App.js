4336449dee663ce8213a63bb740e6adccb63c0a5
XionKannagi <0906chachamaru@gmail.com>
2019-11-17 16:51:43 +0900

    click event implementation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+
+  const increment2 = () => setCount(previousCount => previousCount + 1);
+  const decrement2 = () => setCount(previousCount => previousCount - 1);
+
+  const reset = () => setCount(0);
+
+  const double = () => setCount(count * 2);
+
+  const check3 = () => setCount(count % 3 === 0 ? count / 3: count);
+
   return(
-    <div>
-      This is a Template.
-    </div>
-  )
+    <>
+      <div>count: {count}</div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={increment2}>+1</button>
+        <button onClick={decrement2}>-1</button>
+      </div>
+      <div>
+        <button onClick={reset}>Reset</button>
+        <button onClick={double}>x2</button>
+        <button onClick={check3}>３で割った時の余り</button>
+      </div>
+      
+    </>
+  );
 }
 
 export default App;
