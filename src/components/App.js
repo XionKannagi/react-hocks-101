b9afe6e48474dd3eac51f037b1e8476ac973cd51
XionKannagi <0906chachamaru@gmail.com>
2019-11-17 19:03:43 +0900

    create reducer for

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return(
-    <div>
-      This is a Template.
-    </div>
+    <>
+      <div className='container-fluid'>
+        <h4>イベント作成フォーム</h4>
+        <form>
+          <div className="form-group">
+            <label htmlFor="formEventTitle">タイトル</label>
+            <input className="form-control" id="formEventTitle"/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="formEventBody">ボディー</label>
+            <textarea className="form-control" id="formEventBody"/>
+
+            <button className="btn btn-primary">インベントを作成する</button> 
+            <button className="btn btn-danger">全てのイベントを削除する</button> 
+          </div>
+        </form>
+
+        <h4>イベント一覧</h4>
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Body</th>
+              </tr>
+            </thead>
+            <tbody>
+            </tbody>
+          </table>
+      </div>
+    </>
   )
 }
 
